cae036389ffc5f7c44a094aaa4b58da0ee3e35ac
nick-mcbride <156385638+nick-mcbride@users.noreply.github.com>
2024-10-24 12:46:47 -0400

    update commit

---
+++ Fight Flight/Assets/Scripts/Laser.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject); // Destroy the enemy ship
+            Destroy(gameObject); // Destroy the laser
+        }
+    }
+}
--- Fight Flight/Assets/Scripts/PlayerShooting.cs
+++ Fight Flight/Assets/Scripts/PlayerShooting.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] GameObject laserPrefab; 
-    [SerializeField] Transform firePoint; 
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] Transform firePoint;
     [SerializeField] float fireRate = 0.5f;
 
-    // nextFireTime is used to keep track of when the player can shoot again
     private float nextFireTime = 0f;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetButton("Fire1") && Time.time > nextFireTime)
